5ebfaecde7833b110ad4dc66f7f00edad566a3b9
moonhyunhu <ansgusgn@gmaile.com>
2024-04-16 17:15:34 +0900

    js삭제

--- int/introduction.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $("#introweb").load("introduction.html", function () {
-        //페이지 별로 다른 메뉴 css 적용도 가능
-        document.getElementById("sub_nav").classList.add("show");
-    });
-})
